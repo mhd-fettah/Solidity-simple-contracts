da6346cff4724903088c224babc25a42333e513f
Remix Workspace <>
2022-09-18 06:03:35 +0300

    simple storage

---
+++ simpleStorage.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+//this simple contract store data as int in StoreData var 
+// you can get the data via get() function 
+// and set the data via Set(uint x) function 
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
